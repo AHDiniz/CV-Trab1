642e94f6f49a75155ef01d0551487a1163b55587
Alan H. Diniz <alanhdiniz@gmail.com>
2021-08-12 20:01:49 -0300

    Adding more script files

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import os
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
 
 # Get the images
+
+
 # Apply pre-processing to the images
 # Split the database between training and testing
 # Train the classification model
