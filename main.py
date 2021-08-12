768c1182613ca2d42c4590d62e122d23b8ab002e
Alan Herculano Diniz <alanhdiniz@gmail.com>
2021-08-12 18:15:14 -0300

    Adding image database

---
+++ main.py
@@ -0,0 +1,8 @@
+
+# Get the images
+# Apply pre-processing to the images
+# Split the database between training and testing
+# Train the classification model
+# Get training statistical data
+# Test the classification model
+# Get testing statistical model
