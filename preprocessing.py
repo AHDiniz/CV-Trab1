66c915c87dd74ef1211435e0999483a95fe2ac59
Alan H. Diniz <alanhdiniz@gmail.com>
2021-08-13 14:24:58 -0300

    Implementing preprocessing descriptors

--- preprocessing.py
+++ preprocessing.py
@@ -2,12 +2,17 @@ import os
 import cv2 as cv
 import numpy as np
 from glob import glob
+from skimage.feature import local_binary_pattern
 
 def import_images(animal_dir_names : dict) -> dict:
     result : dict = dict({})
     
     for animal, dir_name in animal_dir_names.items():
-        result[animal] = list(os.listdir('raw-img/' + dir_name))
+        file_list : list = list(os.listdir('raw-img/' + dir_name))
+        result[animal] = list([])
+        for filename in file_list:
+            f = os.path.join('raw-img/' + dir_name, filename)
+            result[animal].append(cv.imread(f))
 
     return result
 
@@ -20,9 +25,25 @@ def preprocess_image(img : np.ndarray) -> np.ndarray:
     img = cv.GaussianBlur(img, (5, 5), 0)
     return img
 
-def feature_extraction() -> (np.ndarray, np.ndarray):
-    # HOG
-    # Haar
-    # SIFT
-    # SURF
+def compute_hog(img : np.ndarray, number_blocks : int = 4, number_bins : int = 12) -> np.ndarray:
+    winSize : tuple = img.shape
+    blockSize : tuple = (int(winSize[0] / number_blocks), int(winSize[1] / number_blocks))
+    blockStride : tuple = blockSize
+    cellSize : tuple = (int(blockSize[0] / 2), int(blockSize[1] / 2))
+    hog : cv.HOGDescriptor = cv.HOGDescriptor(winSize, blockSize, blockStride, cellSize, number_bins)
+    vector : np.ndarray = hog.compute(img)
+    return np.reshape(vector, (vector.shape[0]))
+
+def compute_lbp(img : np.ndarray, radius : int = 1, number_points : int = 8, METHOD : str = 'uniform') -> np.ndarray:
+    lbp = local_binary_pattern(img, number_points, radius, METHOD)
+    hist, _ = np.histogram(lbp.ravel(), bins = np.arrange(0, 255))
+    hist = hist.astype('float')
+    hist /= hist.sum()
+    return hist
+
+def feature_extraction(img_dict : dict) -> (np.ndarray, np.ndarray):
+    for animal, img_list in img_dict.items():
+        for img in img_list:
+            hog_vector = compute_hog(img)
+            lbp_hist = compute_lbp(img)
     pass
